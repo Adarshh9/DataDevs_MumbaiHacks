7db563a9bf82e3a0a8104071f504dc630cbcdc40
Vighnesh Mishra <vighnesh97531@gmail.com>
2023-12-15 13:07:14 +0530

    [Add] Backup for roadmap

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def Landing():
     return("Landing Page")
+
+# Adding blog
+@app.route('/dashboard/user_domain/<domain_name>' )
+def dash_userdomain(domain_name):
+    return("Domain Name" , domain_name)
+
+
 
 if(__name__ == "__main__"):
     app.run(port=8000, debug=True)
